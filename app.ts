e36777c28d90f55051fa07a1254c995a7bed918d
André Pinto <andrepinto.agalijo@gmail.com>
2024-09-13 20:47:20 +0000

    Initial commit

---
+++ app.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import path from "path";
+import bootstrap from "bootstrap";
+
+const app = express();
+const port = 3000;
+
+// * SERVE THE PUBLIC DIRECTORY
+const publicDir = express.static(path.join(__dirname, "/public"));
+app.use(publicDir);
+
+// * SERVE BOOTSTRAP, JQUERY & POPPER
+const bsCssDir = express.static(
+  path.join(__dirname, "/node_modules/bootstrap/dist/css")
+);
+
+const bsJsDir = express.static(
+  path.join(__dirname, "/node_modules/bootstrap/dist/js")
+);
+
+const jqDir = express.static(
+  path.join(__dirname, "/node_modules/jquery/dist/")
+);
+
+const popperJs = express.static(
+  path.join(__dirname, "/node_modules/@popperjs/core/dist/umd")
+);
+
+app.use(bsCssDir);
+app.use(bsJsDir);
+app.use(jqDir);
+app.use(popperJs);
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`Sandbox listening on port ${port}`);
+});
