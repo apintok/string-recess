05e813e7c6870027b37c6b13e71376a3cc755355
Andre Pinto <andrepinto@rsult.com>
2024-09-17 22:04:10 +0200

    project structure. Adding ejs, sass and bootstrap

--- app.ts
+++ app.ts
@@ -1,40 +1,28 @@
-import express from "express";
-import path from "path";
-import bootstrap from "bootstrap";
+import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
 // * SERVE THE PUBLIC DIRECTORY
-const publicDir = express.static(path.join(__dirname, "/public"));
-app.use(publicDir);
+const publicDir = express.static(path.join(__dirname, '/public'));
 
 // * SERVE BOOTSTRAP, JQUERY & POPPER
-const bsCssDir = express.static(
-  path.join(__dirname, "/node_modules/bootstrap/dist/css")
-);
+const bsCssDir = express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css'));
+const bsJsDir = express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js'));
+// const jqDir = express.static(path.join(__dirname, '/node_modules/jquery/dist/'));
+// const popperJs = express.static(path.join(__dirname, '/node_modules/@popperjs/core/dist/umd'));
 
-const bsJsDir = express.static(
-  path.join(__dirname, "/node_modules/bootstrap/dist/js")
-);
-
-const jqDir = express.static(
-  path.join(__dirname, "/node_modules/jquery/dist/")
-);
-
-const popperJs = express.static(
-  path.join(__dirname, "/node_modules/@popperjs/core/dist/umd")
-);
-
+app.use(publicDir);
 app.use(bsCssDir);
 app.use(bsJsDir);
-app.use(jqDir);
-app.use(popperJs);
+// app.use(jqDir);
+// app.use(popperJs);
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 app.listen(port, () => {
